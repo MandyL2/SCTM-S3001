307eca0d2159f06657b9ea17501e630ba8e5b263
MandyL2 <fioleewriter@gmail.com>
2022-10-24 22:51:05 -0400

    initial set up

---
+++ musicLibrary.js
@@ -0,0 +1,20 @@
+//professor layton music library
+/**
+ * Game Series
+ * Image
+ * Title
+ * Song url
+ *
+ *
+ * Spotify library set up
+ * album for each game
+ * 3 songs each album
+ * sidebar nav
+ */
+
+let albums = [
+	{
+		album_name: "",
+		img: "",
+	},
+];
